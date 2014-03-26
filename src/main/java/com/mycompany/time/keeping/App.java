9a1638859ccbcd53687e080d02576c934e93b7ca
By Heart Learner <byheartlearner@gmail.com>
2014-03-26 12:40:54 +0530

    adding new proj to github

---
+++ src/main/java/com/mycompany/time/keeping/App.java
@@ -0,0 +1,13 @@
+package com.mycompany.time.keeping;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
